48dd62cd5452000224bd88f93c21e3c3c34b78ad
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-02-01 13:21:07 -0600

    Initial Commit

---
+++ finiteSumSin/finiteSumSinApp.swift
@@ -0,0 +1,17 @@
+//
+//  finiteSumSinApp.swift
+//  finiteSumSin
+//
+//  Created by IIT PHYS 440 on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct finiteSumSinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
